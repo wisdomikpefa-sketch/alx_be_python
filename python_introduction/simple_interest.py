5730ee2a42f0365bed5ac217e2309a5b630f6d26
wisdom <wisdomikpefa@gmail.com>
2025-09-21 04:39:12 +0100

    main

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+
+principal = 1000
+rate = 0.05
+time = 3
+
+print("principal:", principal)
+print("rate:", rate)
+print("time:", time)
+simple_interest = principal * rate * time
+print("simple_interest:", simple_interest)
