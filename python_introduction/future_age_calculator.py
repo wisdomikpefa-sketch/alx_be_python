501abaeae6b1cc26e167011c8049249152e52c67
wisdom <wisdomikpefa@gmail.com>
2025-09-21 12:11:01 +0100

    main

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+# print(int(input(how old are you?)))
+# age = input
+# print("in 2050, you will be", age, "years old?")
+# 
+age = int(input("how old are you?,"))
+print("in 2050, you will be", age + 27, "years old?")
