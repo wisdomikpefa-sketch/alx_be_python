456aca5afd7e1cf13d46a6e7c2d681b6004a6c53
wisdom <wisdomikpefa@gmail.com>
2025-10-04 14:55:40 +0100

    main

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,11 +1,19 @@
-# Simple multiplication calculator
-
 num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-operation = input("Choose the operation (*, ): ")
-
-if operation == '*':
-    result = num1 * num2
-    print("Result:", result)
-else:
-    print("Cannot divide by zero.")
+num2 = float(input("Enter the second number: ")) 
+operation = input("Choose the operation (+, -, *, /) ")
+match operation:
+    case "+":
+        result = num1 + num2
+        print(f"The result is {result}")
+    case "-":
+        result = num1 - num2
+        print(f"The result is {result}")
+    case "*":
+        result = num1 * num2
+        print(f"The result is {result}")
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}")
