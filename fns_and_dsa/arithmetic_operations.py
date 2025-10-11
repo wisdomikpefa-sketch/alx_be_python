5d2193332d3e3a73994ba47424daf4fe02e320a1
wisdom <wisdomikpefa@gmail.com>
2025-10-11 15:13:16 +0100

    main

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,10 +2,7 @@ def perform_operation():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-
-    #result = num1 + num2 
-    #print(f"Result: {result}")
-
+    
     if operation == "add":
         result = num1 + num2
         print(f"Result: {result}")
@@ -24,6 +21,3 @@ def perform_operation():
         else:
             result = num1 / num2
             print(f"Result: {result}")
-
-# if __name__ == "__main__":
-perform_operation()
