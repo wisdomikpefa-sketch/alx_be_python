2226fd4d97109c37d48c6af4d335b513ab6ef485
wisdom <wisdomikpefa@gmail.com>
2025-10-12 17:47:42 +0100

    main

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,10 +1,11 @@
 # temp_conversion_tool.py
+#fahrenheit_to_celsius_factor
+#celsius_to_fahrenheit_factor
 
 # Global conversion factors
-
 FAHRENHEIT_OFFSET = 32
-FAHRENHEIT_TO_CELSIUS_FACTOR = 9 / 5
-CELSIUS_TO_FAHRENHEIT_FACTOR = 5 / 9
+FAHRENHEIT_SCALE = 9 / 5
+CELSIUS_SCALE = 5 / 9
 
 def celsius_to_fahrenheit_factor(celsius):
     """Convert Celsius to Fahrenheit using global variables."""
